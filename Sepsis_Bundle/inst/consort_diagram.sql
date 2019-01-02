4fef76ca8ed37911b52ef304fd5f70ea159b0cf5
xing song <sxingerl@gmail.com>
2019-01-01 18:55:49 -0600

    updated consort diagram

--- Sepsis_Bundle/inst/consort_diagram.sql
+++ Sepsis_Bundle/inst/consort_diagram.sql
@@ -165,91 +165,109 @@ select 26 as idx,'IV triggered - Non-Sepsis' CNT_TYPE,
 from ED_SI_SEPSIS_STAGE
 where iv_trigger_since_triage is not null and sepsis_ind=0
 union all
-select 27 as idx,'IV completed - Non-Sepsis' CNT_TYPE,
+select 27 as idx,'IV initiated - Non-Sepsis' CNT_TYPE,
+       count(distinct patient_num) PAT_CNT,
+       count(distinct encounter_num) ENC_CNT
+from ED_SI_SEPSIS_STAGE
+where coalesce(IV_0_since_triage,IV_1_since_triage,IV_2_since_triage,IV_3_since_triage) is not null and sepsis_ind=0
+union all
+select 28 as idx,'IV completed - Non-Sepsis' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
 where iv_complt_since_triage is not null and sepsis_ind=0
 union all
-select 28 as idx,'IV triggered - Sepsis' CNT_TYPE,
+select 29 as idx,'IV triggered - Sepsis' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
 where iv_trigger_since_triage is not null and sepsis_ind=1
 union all
-select 29 as idx,'IV initiated - Sepsis' CNT_TYPE,
+select 30 as idx,'IV initiated - Sepsis' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
-where IV_0_since_triage is not null and sepsis_ind=1
+where coalesce(IV_0_since_triage,IV_1_since_triage,IV_2_since_triage,IV_3_since_triage) is not null and sepsis_ind=1
 union all
-select 30 as idx,'Sepsis before IV triggered' CNT_TYPE,
+select 31 as idx,'IV completed - Sepsis' CNT_TYPE,
+       count(distinct patient_num) PAT_CNT,
+       count(distinct encounter_num) ENC_CNT
+from ED_SI_SEPSIS_STAGE
+where iv_complt_since_triage is not null and sepsis_ind=1
+union all
+select 32 as idx,'Sepsis before IV triggered' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
 where iv_trigger_since_triage is not null and sepsis_ind=1 and sepsis_since_triage < iv_trigger_since_triage
 union all
-select 31 as idx,'Sepsis before IV initiated' CNT_TYPE,
+select 33 as idx,'Sepsis before IV initiated' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
 where IV_0_since_triage is not null and sepsis_ind=1 and sepsis_since_triage < IV_0_since_triage
 union all
-select 32 as idx,'Sepsis before IV completed' CNT_TYPE,
+select 34 as idx,'Sepsis before IV completed' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
 where iv_complt_since_triage is not null and sepsis_ind=1 and sepsis_since_triage < iv_complt_since_triage
 union all
-select 33 as idx,'Sepsis before Antibiotics' CNT_TYPE,
+select 35 as idx,'Sepsis before Antibiotics' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
 where sepsis_ind=1 and abx_since_triage > sepsis_since_triage
 union all
-select 34 as idx,'Hypotensive-Sepsis before Antibiotics' CNT_TYPE,
+select 36 as idx,'Hypotensive-Sepsis before Antibiotics' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
 where sepsis_ind=1 and abx_since_triage > sepsis_since_triage and iv_trigger_since_triage is not null
 union all
-select 35 as idx,'Sepsis before Therapautic Components' CNT_TYPE,
+select 37 as idx,'NonHypotensive-Sepsis before Antibiotics' CNT_TYPE,
+       count(distinct patient_num) PAT_CNT,
+       count(distinct encounter_num) ENC_CNT
+from ED_SI_SEPSIS_STAGE
+where sepsis_ind=1 and abx_since_triage > sepsis_since_triage and iv_trigger_since_triage is null
+union all
+select 38 as idx,'Sepsis before Therapautic Components' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
 where sepsis_ind=1 and least(abx_since_triage,coalesce(IV_0_since_triage,abx_since_triage)) > sepsis_since_triage
 union all
-select 36 as idx,'Hypotensive-Sepsis before Therapautic Components' CNT_TYPE,
+select 39 as idx,'Hypotensive-Sepsis before Therapautic Components' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
 where sepsis_ind=1 and least(abx_since_triage,coalesce(IV_0_since_triage,abx_since_triage)) > sepsis_since_triage and iv_trigger_since_triage is not null
 union all
-select 37 as idx,'NonHypotensive-Sepsis before Therapautic Components' CNT_TYPE,
+select 40 as idx,'NonHypotensive-Sepsis before Therapautic Components' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
 where sepsis_ind=1 and least(abx_since_triage,coalesce(IV_0_since_triage,abx_since_triage)) > sepsis_since_triage and iv_trigger_since_triage is null
 union all
-select 38 as idx,'Repeat Lactate' CNT_TYPE,
+select 41 as idx,'Repeat Lactate' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
 where LACTATE2_since_triage is not null
 union all
-select 39 as idx,'Remain Hypotensive' CNT_TYPE,
+select 42 as idx,'Remain Hypotensive' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
 where hypot2_since_trigger is not null
 union all
-select 40 as idx,'Vasopressor initiated' CNT_TYPE,
+select 43 as idx,'Vasopressor initiated' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
 where pressor_since_triage is not null
 union all
-select 41 as idx,'Cardiac events' CNT_TYPE,
+select 44 as idx,'Cardiac events' CNT_TYPE,
        count(distinct patient_num) PAT_CNT,
        count(distinct encounter_num) ENC_CNT
 from ED_SI_SEPSIS_STAGE
